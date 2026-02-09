1eacbddfc73ff83f8e9f6e79232e2cffc2cb419f
Shash-135 <shashanktalekar3@gmail.com>
2026-02-09 19:11:36 +0530

    Read email config from .env

--- synca_project/settings.py
+++ synca_project/settings.py
@@ -75,13 +75,13 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='shashanktalekar7@gmail.com'
-EMAIL_HOST_PASSWORD='xwbl vgyr kjsn keom'
-EMAIL_USE_TLS=True
-DEFAULT_FROM_EMAIL='Synca <shashanktalekar7@gmail.com>'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER or 'no-reply@synca.local')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
